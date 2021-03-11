5832912fca489e57ec27ae838a6a92a6b68a710b
daletom <tom@imgix.com>
2021-03-10 23:33:32 -0800

    iniitial commit

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
-import Head from "next/head";
 import React from "react";
-import { Image, StructuredText, renderMetaTags, renderMetaTagsToString } from "react-datocms";
+import { Image } from "react-datocms";
 import { query } from "../lib/query";
 import { request } from "../lib/datocms";
 
 export async function getStaticProps() {
   const data = await request({
     query,
-    variables: { first: 10 },
+    variables: { first: 100 },
   });
 
   return {
@@ -16,53 +15,63 @@ export async function getStaticProps() {
 }
 
 const App = ({ data }) => {
-  const metaTags = data.page.seo.concat(data.site.favicon);
-
   return (
-    <div>
-      <Head>{renderMetaTags(metaTags)}</Head>
-      <pre className="seo-inspect">
-        Look at all these juicy meta tags! ↴
-        <br />
-        <br />
-        {renderMetaTagsToString(metaTags)}
-      </pre>
-      <div className="app">
-        <div className="app-title">DatoCMS Blog</div>
-        <div className="app-subtitle">
-          News, tips, highlights, and other updates from the team at DatoCMS.
-        </div>
-        {data.blogPosts.map((blogPost) => (
-          <article key={blogPost.id} className="blogPost">
-            <Image
-              className="blogPost-image"
-              fadeInDuration={1000}
-              data={blogPost.coverImage.responsiveImage}
-            />
-            <h6 className="blogPost-title">
-              <a
-                href={`https://www.datocms.com/blog/${blogPost.slug}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {blogPost.title}
-              </a>
-            </h6>
-            <div className="blogPost-excerpt">
-              <StructuredText data={blogPost.excerpt} />
-            </div>
-            <footer className="blogPost-author">
-              <Image
-                className="blogPost-author-image"
-                data={blogPost.author.avatar.responsiveImage}
+    <div class="p-4"> 
+      <picture class="h-auto w-full">
+      <source
+        media="(min-width: 2100px)"
+        srcset="https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=2400&fit=crop&ar=4%3A1&txt64=RmFyIEZhciBBd2F5IE5ld3M&txt-color=white&txt-align=middle%2Ccenter&txt-fit=max&txt-size=72&auto=format,compress"
               />
-              Written by {blogPost.author.name}
-            </footer>
-          </article>
-        ))}
+      <source
+        media="(min-width: 1800px)"
+        srcset="https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=2100&fit=crop&ar=4%3A1&txt64=RmFyIEZhciBBd2F5IE5ld3M&txt-color=white&txt-align=middle%2Ccenter&txt-fit=max&txt-size=72&auto=format,compress"
+              />
+      <source
+        media="(min-width: 1600px)"
+        srcset="https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=1800&fit=crop&ar=4%3A1&txt64=RmFyIEZhciBBd2F5IE5ld3M&txt-color=white&txt-align=middle%2Ccenter&txt-fit=max&txt-size=72&auto=format,compress"
+              />
+      <source
+        media="(min-width: 1400px)"
+        srcset="https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=1600&fit=crop&ar=4%3A1&txt64=RmFyIEZhciBBd2F5IE5ld3M&txt-color=white&txt-align=middle%2Ccenter&txt-fit=max&txt-size=72&auto=format,compress"
+              />
+      <source
+        media="(min-width: 1200px)"
+        srcset="https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=1400&fit=crop&ar=4%3A1&txt64=RmFyIEZhciBBd2F5IE5ld3M&txt-color=white&txt-align=middle%2Ccenter&txt-fit=max&txt-size=72&auto=format,compress"
+              />
+      <source
+        media="(min-width: 900px)"
+        srcset="https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=1200&fit=crop&ar=3%3A1&txt64=RmFyIEZhciBBd2F5IE5ld3M&txt-color=white&txt-align=middle%2Ccenter&txt-fit=max&txt-size=72&auto=format,compress"
+              />
+      <source
+        media="(min-width: 600px)"
+        srcset="https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=900&fit=crop&ar=3%3A1&txt64=RmFyIEZhciBBd2F5IE5ld3M&txt-color=white&txt-align=middle%2Ccenter&txt-fit=max&txt-size=72&auto=format,compress"
+              />
+      <source
+        media="(min-width: 450px)"
+        src="https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=600&fit=crop&ar=1%3A1&txt64=RmFyIEZhciBBd2F5IE5ld3M&txt-color=white&txt-align=middle%2Ccenter&txt-fit=max&txt-size=72&auto=format,compress"
+              />
+      <source
+        media="(min-width: 1px)"
+        srcset="https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=550&fit=crop&ar=1%3A1&txt64=RmFyIEZhciBBd2F5IE5ld3M&txt-color=white&txt-align=middle%2Ccenter&txt-fit=max&txt-size=72&auto=format,compress"
+              />
+      <img width="1200" height="600" src="https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=1200&fit=crop&ar=2%3A1&txt64=RmFyIEZhciBBd2F5IE5ld3M&txt-color=white&txt-align=middle%2Ccenter&txt-fit=max&txt-size=72&auto=format,compress"></img>
+    </picture>
+      <div class="flex flex-wrap">
+      {data.blogPosts.map((blogPost) => (
+        <div class="p-4 lg:w-1/3 md:w-1/2 sm:w-full">
+        {/* <Link to={node.slug}> */}
+          <p class="text-2xl p-2">{blogPost.title}</p>
+          {/* </Link> */}
+          <Image
+            data={blogPost.heroimage.responsiveImage}
+            sizes='(min-width: 1024px) calc(30vw - 128px), (min-width: 768px) calc(50vw - 100px), calc(100vw - 70px)'
+          />
+          <p class="p-2">{blogPost.content}</p>
+        </div>
+      ))}
       </div>
-    </div>
-  );
-};
+      </div>
+      )
+  }
 
 export default App;
